714cc604e9de489e0124e1ef48017b8fe02ea72f
niklaux <nlocer97@gmail.com>
2023-02-02 01:22:47 +0800

    Exercise 5 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  if (num1 < 0 || num2 < 0 || Number(num1) !== num1 || Number(num2) !== num2) {
+    return "ERROR";
+  }
 
+  if (num1 > num2) {
+    temp = num1;
+    num1 = num2;
+    num2 = temp;
+  }
+  
+  let sum = 0;
+  for (num1; num1 <= num2 ; num1++) {
+    sum += num1;
+  }
+  return sum;
 };
 
 // Do not edit below this line
