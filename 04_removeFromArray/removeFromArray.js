b3015e377dff1a00cac86a7943039d5f0ac28f57
niklaux <nlocer97@gmail.com>
2023-02-02 10:25:14 +0800

    Exercise 4 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  const result = array.filter((item) => {
+    return !args.includes(item);
+  });
 
+  return result;
 };
 
 // Do not edit below this line
